a64b7ba864aeb82b9cf49ba26f6b195457c70d2a
sripadaraj <sripadaraj183@gmail.com>
2017-03-26 16:23:15 +0530

    Create status_dumy.go

---
+++ cmd/status_dumy.go
@@ -0,0 +1,16 @@
+Package main 
+import 
+(
+  “fmt”
+“bytes’
+
+“github.com/mitchellh/cli”
+)
+
+func main()
+var cmd string
+fmt.scanf(%s”,&cmd)
+fmt.printf(“%-12s%-12s%-12%-12s”,”service”,”ip”,”port”,”status”)
+fmt.println()
+fmt.printf(“%-12s%-12s%-12%-12s”,”mayaserver”,”10.10.10.10”,”5656”,”running”)
+}
