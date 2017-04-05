7575807eec0d66113bc1d4ad346c79a4ea3e5cc6
sripadaraj <sripadaraj183@gmail.com>
2017-04-06 04:52:20 +0530

    Update status.go

--- cmd/status.go
+++ cmd/status.go
@@ -8,16 +8,19 @@ import (
   
 	"sort"
 	"strconv"
-  "strings"
-  "os"
+	"string"
+  	"os"
 	"github.com/openebs/mayaserver/lib/config"
-
+//using maya configuration can print the bind addr and nodes name
 	"github.com/mitchellh/cli"
+//cli imports all required cli-ui commands
 )
   
   type Ports struct {
 	HTTP int `mapstructure:"http"`
 }
+//this structure helps in managing the ports
+//also supports default ports to server
 
 type StatusCommand struct {
 	Meta
@@ -29,13 +32,13 @@ type StatusCommand struct {
 	Datacenter string
 	LogLevel   string
   }
-  
+// status structure which includes the cofig nodes
 
 func usage() {
-    fmt.Fprintf(os.Stderr, "usage: test-local-port [5656\n")
+    fmt.Fprintf(os.Stderr, "usage: test-local-port [5656]\n")
     flag.PrintDefaults()
     os.Exit(2)
-}
+} //this function returns the actual port usage
 
 
 func (f *ports)  readMayaConfig() *config.MayaConfig {
@@ -49,33 +52,26 @@ func (f *ports)  readMayaConfig() *config.MayaConfig {
   args := flag.Args()
 }
 
+	
+//check with the port details to know if the port is used or not	
 func (f *ports) check() *Statuscommand {
 
 if len(args) < 1 {
       fmt.Fprintf(os.Stderr, "Input port is missing.")
       os.Exit(1)
-}
+}//checks the input port 
 
 if  port := args[0]
   _, err := strconv.ParseUint(port, 10, 16)
+	//this coverts the string to uint and compares with the actual expected port
   if err != nil {
     fmt.Fprintf(os.Stderr, "Invalid port %q: %s\n", port, err)
     os.Exit(1)
-  {
-      func (mc *MayaConfig) Listener(proto, addr string, port int) (net.Listener, error) {
-if 0 > port || port > 65535 {
-		return nil, &net.OpError{
-			Op:  "listen",
-			Net: proto,
-			Err: &net.AddrError{Err: "invalid port", Addr: fmt.Sprint(port)},
-	  	  }
-	    }
-	return net.Listen(proto, net.JoinHostPort(addr, strconv.Itoa(port)))
     }
   }
 }
 
-
+//using merge function in configuration to merge actual port and the expected port
 func (mc *MayaConfig) Merge(b *MayaConfig) *MayaConfig {
 if result.Ports == nil && b.Ports != nil {
 		ports := *b.Ports
@@ -84,7 +80,7 @@ if result.Ports == nil && b.Ports != nil {
 		result.Ports = result.Ports.Merge(b.Ports)
 }
 
-
+//merge function 
 func (a *Ports) Merge(b *Ports) *Ports {
 	result := *a
 
@@ -94,6 +90,7 @@ func (a *Ports) Merge(b *Ports) *Ports {
 	return &result
 }
 
+	//prints the cofiguration of status 
 func (c *StatusCommand) readMayaConfig() *config.MayaConfig {
 
  {
@@ -111,10 +108,15 @@ func (c *StatusCommand) readMayaConfig() *config.MayaConfig {
   
   }
   
+	//the main status command
+	//here when the vagrant machine is running
+	//then the status runs
   func (c *StatusCommand) Run(args []string) int {
-   
+   //check for vagrant up
+	  //because when vagrant up is not done then the m-apiserver could not be responded
   if config.vmbox_status == 'running'
  {
+	 //assiging the up variable the value of up command to check
  up := {func() (cli.Command, error) {
 			return &cmd.UpCommand{
 				Revision:          GitCommit,
@@ -124,12 +126,17 @@ func (c *StatusCommand) readMayaConfig() *config.MayaConfig {
 				ShutdownCh:        make(chan struct{}),
 }, nil
 }
-
+//CMD UP is mot nil then the status command can be executed 
+	//up variable either stores the nil | up terms 
+	//when it is not nil the status command can be executed
 if up != nil {
 
   mconfig := c.readMayaConfig() 
 
    expec_port= check () *Statuscommand 
+	//the merged ports can be verfied when merged the ports may be sam as expected port and the port running
+	//by this check the code can be runned 
+	//when it is not running on the actual/expected port he status command cannot be executed
     if expec_port=='5656'
     {
      c.Ui.Output(out)
